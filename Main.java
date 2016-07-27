ac2d0bdb86d7ceeab3dc444fdfc9e87d4835eaef
陳楷文 <kelvin91734@yahoo.com.tw>
2016-07-27 12:20:08 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+package note3;
+
+public class Main {
+	public static void main(String args[]){
+		System.out.println("Note 3: 轉義字符");
+		System.out.println("轉義字符為輸入字符的十六進位而非字符本身來代替");
+		System.out.println("如Unicode編碼為反斜線+u開頭");
+		System.out.println("這是\u03c0 編碼為03c0");
+		System.out.println("這是\u03a0 編碼為03a0");
+	}
+}
